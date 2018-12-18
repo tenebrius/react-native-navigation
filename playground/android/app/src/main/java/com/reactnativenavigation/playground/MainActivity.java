6e3dca7358df294a3d7b08b8b54ea7fe803cf811
Guy Carmeli <guyc@wix.com>
2018-12-18 10:03:23 +0200

    Minor cleanup

--- playground/android/app/src/main/java/com/reactnativenavigation/playground/MainActivity.java
+++ playground/android/app/src/main/java/com/reactnativenavigation/playground/MainActivity.java
@@ -1,6 +1,5 @@
 package com.reactnativenavigation.playground;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.widget.ImageView;
@@ -19,16 +18,5 @@ public class MainActivity extends NavigationActivity {
         ImageView img = new ImageView(this);
         img.setImageDrawable(getDrawable(R.drawable.ic_android));
         setContentView(img);
-    }
-
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        dismissSystemAlertsToPreventDetoxFromTimingOut(hasFocus);
-    }
-
-    private void dismissSystemAlertsToPreventDetoxFromTimingOut(boolean hasFocus) {
-        if (! hasFocus) {
-            sendBroadcast(new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS));
-        }
     }
 }
