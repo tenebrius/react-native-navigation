05dacbd0729f4ebf0074bd21c50f3bf813ad7fab
Guy Carmeli <guyc@wix.com>
2019-01-07 13:08:42 +0200

    Set elevation 0 when creating TopBar
    Apparently TopBar elevation property is animated. Meaning that if 4dp elevation is set, when the
    view appears its elevation is animated from 0dp to 4dp.
    This resulted in rare race condition where the onAnimationEnd callback was called after
    we set the desired elevation value, and an unexpected elevation value was set.

--- lib/android/app/src/main/java/com/reactnativenavigation/viewcontrollers/topbar/TopBarController.java
+++ lib/android/app/src/main/java/com/reactnativenavigation/viewcontrollers/topbar/TopBarController.java
@@ -1,6 +1,7 @@
 package com.reactnativenavigation.viewcontrollers.topbar;
 
 import android.content.Context;
+import android.os.Build;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 
@@ -10,12 +11,14 @@ import com.reactnativenavigation.views.topbar.TopBar;
 
 
 public class TopBarController {
+    private static final int INITIAL_ELEVATION = 0;
     private TopBar topBar;
 
     public View createView(Context context, StackLayout stackLayout) {
         if (topBar == null) {
             topBar = createTopBar(context, stackLayout);
             topBar.setId(CompatUtils.generateViewId());
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) topBar.setElevation(INITIAL_ELEVATION);
         }
         return topBar;
     }
--- lib/android/app/src/test/java/com/reactnativenavigation/viewcontrollers/TopBarControllerTest.java
+++ lib/android/app/src/test/java/com/reactnativenavigation/viewcontrollers/TopBarControllerTest.java
@@ -2,7 +2,6 @@ package com.reactnativenavigation.viewcontrollers;
 
 import android.app.Activity;
 import android.content.Context;
-import android.support.annotation.NonNull;
 
 import com.reactnativenavigation.BaseTest;
 import com.reactnativenavigation.viewcontrollers.topbar.TopBarController;
@@ -20,31 +19,38 @@ import static org.mockito.Mockito.verify;
 public class TopBarControllerTest extends BaseTest {
 
     private TopBarController uut;
+    private TitleBar titleBar;
+    private TopBar topBar;
+    private Activity activity;
 
     @Override
     public void beforeEach() {
-        uut = new TopBarController();
+        activity = newActivity();
+        uut = new TopBarController() {
+            @Override
+            protected TopBar createTopBar(Context context, StackLayout stackLayout) {
+                topBar = spy(new TopBar(context, stackLayout) {
+                    @Override
+                    protected TitleBar createTitleBar(Context context) {
+                        titleBar = spy(super.createTitleBar(context));
+                        return titleBar;
+                    }
+                });
+                return topBar;
+            }
+        };
+    }
+
+    @Test
+    public void createView_setElevationToCancelDefaultElevationAnimationWhichMightConflictWithElevationValueFromDefaultOptions() {
+        uut.createView(activity, Mockito.mock(StackLayout.class));
+        verify(topBar).setElevation(0);
     }
 
     @Test
     public void clear() {
-        final TitleBar[] titleBar = new TitleBar[1];
-        uut = new TopBarController() {
-            @NonNull
-            @Override
-            protected TopBar createTopBar(Context context, StackLayout stackLayout) {
-                return new TopBar(context, stackLayout) {
-                    @Override
-                    protected TitleBar createTitleBar(Context context) {
-                        titleBar[0] = spy(super.createTitleBar(context));
-                        return titleBar[0];
-                    }
-                };
-            }
-        };
-        Activity activity = newActivity();
         uut.createView(activity, Mockito.mock(StackLayout.class));
         uut.clear();
-        verify(titleBar[0], times(1)).clear();
+        verify(titleBar, times(1)).clear();
     }
 }
