5566ffd47c65f7bfc608f3a0f0b19814039b541e
David undefined <offantik@gmail.com>
2019-05-15 09:33:23 +0300

    Refactor TopBarOptions (#5063)

--- lib/src/interfaces/Options.ts
+++ lib/src/interfaces/Options.ts
@@ -327,7 +327,11 @@ export interface OptionsTopBar {
   /**
    * Change button colors in the top bar
    */
-  buttonColor?: Color;
+
+  leftButtonColor?: Color;
+  rightButtonColor?: Color;
+  leftButtonDisabledColor?: Color;
+  rightButtonDisabledColor?: Color;
   /**
    * Draw behind the navbar
    */
