d5c92b1609d5fc1a75c51065791e5aad2a80b654
Danilo Bürger <info@danilobuerger.de>
2019-09-05 11:06:33 +0200

    Stop recursive double setting of default options (#5456)
    There is no need to call `setDefaultOptions` on the `childViewController` while iterating the `childViewControllers` as those will be passed recursively as `rootViewController` anyway and thus calling `setDefaultOptions` in the nested recursive call.

--- lib/ios/RNNDefaultOptionsHelper.m
+++ lib/ios/RNNDefaultOptionsHelper.m
@@ -3,14 +3,11 @@
 @implementation RNNDefaultOptionsHelper
 
 + (void)recrusivelySetDefaultOptions:(RNNNavigationOptions *)defaultOptions onRootViewController:(UIViewController *)rootViewController {
-	if ([rootViewController conformsToProtocol:@protocol(RNNLayoutProtocol)])
+	if ([rootViewController conformsToProtocol:@protocol(RNNLayoutProtocol)]) {
 		[((UIViewController<RNNLayoutProtocol> *)rootViewController) setDefaultOptions:defaultOptions];
+	}
 	
 	for (UIViewController<RNNLayoutProtocol>* childViewController in rootViewController.childViewControllers) {
-		if ([childViewController conformsToProtocol:@protocol(RNNLayoutProtocol)]) {
-			[childViewController setDefaultOptions:defaultOptions];
-		}
-		
 		[self recrusivelySetDefaultOptions:defaultOptions onRootViewController:childViewController];
 	}
 }
