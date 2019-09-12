16646e7c88d78f1ddd7fb6ae434ef968ac051f06
Ward Abbass <warda@wix.com>
2019-09-12 12:12:00 +0300

    Add support for getLaunchArgs on Android (#5466)
    Add support for getLaunchArgs on Android
    
    **Usage:** 
    
    1. **Pass** args in order to run the application on the device using ADB or xcrun or Detox:
        - Detox: `device.launchApp(bundleId,{launchArgs: {'-key1': 'val1'}})` must use launchArgs key, see [docs](https://github.com/wix/Detox/blob/master/docs/APIRef.DeviceObjectAPI.md#7-additional-launch-arguments).
       - ADB: `adb shell am start -n com.yourpackage/com.yourpackage.YourActivity --es launchArgs "{some stringyfied json}"`
       - Xcrun: `/usr/bin/xcrun simctl launch ${udid} ${bundleId} --args -key1 value1 -key2 value2`
    
    2. **Retrieve** the arguments: `const launchArgs = await Navigation.getLaunchArgs();`
       - There is a small difference between platforms on how they pass the launch args:
           - on iOS, the args will be passed as an array of pairs (argv,argc)
           - on Android, the args will be passed in the form of a dictionary 
    
    
    Extract args as a dictionary example:
    ```javascript
    const launchArgs = await Navigation.getLaunchArgs();
    let resultArgs = {};
        if (Platform.OS === 'android') {
          resultArgs = launchArgs; // no need to proccess its a dictionary 
        } else {
          for (let i = 0; i < launchArgs.length; i += 2) {
            if (i + 1 < launchArgs.length) {
              resultArgs[launchArgs[i]] = launchArgs[i + 1];
            }
          }
        }
    ```

--- lib/android/app/src/main/java/com/reactnativenavigation/react/NavigationModule.java
+++ lib/android/app/src/main/java/com/reactnativenavigation/react/NavigationModule.java
@@ -1,5 +1,6 @@
 package com.reactnativenavigation.react;
 
+import com.reactnativenavigation.utils.LaunchArgsParser;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
@@ -72,6 +73,11 @@ public class NavigationModule extends ReactContextBaseJavaModule {
         return NAME;
     }
 
+    @ReactMethod
+    public void getLaunchArgs(String commandId, Promise promise) {
+        promise.resolve(LaunchArgsParser.parse(activity()));
+    }
+    
     @ReactMethod
     public void getConstants(Promise promise) {
         ReactApplicationContext ctx = getReactApplicationContext();
