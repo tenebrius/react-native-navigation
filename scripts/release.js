0a8bbd4e7e60d2769bfd41a855e8aaab0cd5b829
yogevbd <yogev132@gmail.com>
2019-09-15 16:35:27 +0300

    Use BRANCH jenkins variable in release script

--- scripts/release.js
+++ scripts/release.js
@@ -8,7 +8,7 @@ const path = require('path');
 // Workaround JS
 const isRelease = process.env.RELEASE_BUILD === 'true';
 
-const BRANCH = process.env.GIT_BRANCH;
+const BRANCH = process.env.BRANCH;
 const VERSION_TAG = isRelease ? 'latest' : 'snapshot';
 const VERSION_INC = 'patch';
 
