9efd84c54ee72f8779bf38b92c6454f8c8738ca7
d4vidi <amit.d4vidi@gmail.com>
2019-11-27 14:51:20 +0200

    Fix topBar button colors on iOS 13 (#5690)

--- e2e/AndroidUtils.js
+++ e2e/AndroidUtils.js
@@ -1,37 +1,13 @@
 const exec = require('shell-utils').exec;
-
-module.exports = {
-  pressBack: () => {
-    exec.execSync('adb shell input keyevent 4');
-  },
-  pressMenu: () => {
-    exec.execSync('adb shell input keyevent 82');
-  },
-  pressEnter: (keyCode) => {
-    exec.execSync(`adb shell input keyevent 66`);
-  },
-  pressLockButton: () => {
-    exec.execSync(`adb shell input keyevent 26`);
-  },
-  pressKeyCode: (keyCode) => {
-    exec.execSync(`adb shell input keyevent ${keyCode}`);
-  },
-  grantPermission: () => {
-    exec.execSync('adb shell pm grant com.reactnativenavigation.playground android.permission.READ_PHONE_STATE');
-  },
-  revokePermission: () => {
-    exec.execSync('adb shell pm revoke com.reactnativenavigation.playground android.permission.READ_PHONE_STATE');
-  },
-  openActivity: () => {
-    exec.execSync('adb shell am start -n com.reactnativenavigation.playground/.MainActivity');
-  },
+const utils = {
+  pressBack: () => utils.pressKeyCode(4),
+  pressMenu: () => utils.pressKeyCode(82),
+  pressKeyCode: (keyCode) => utils.executeShellCommand(`input keyevent ${keyCode}`),
+  grantPermission: () => utils.executeShellCommand('pm grant com.reactnativenavigation.playground android.permission.READ_PHONE_STATE'),
+  revokePermission: () => utils.executeShellCommand('pm revoke com.reactnativenavigation.playground android.permission.READ_PHONE_STATE'),
   executeShellCommand: (command) => {
-    exec.execSync(`adb shell ${command}`);
+    exec.execSync(`adb -s ${device.id} shell ${command}`);
   },
-  enterText: (text) => {
-    exec.execSync(`adb shell input text ${text}`);
-  },
-  swipeUp: () => {
-    exec.execSync('adb shell input touchscreen swipe 300 1200 500 0 100');
-  }
 };
+
+module.exports = utils;
