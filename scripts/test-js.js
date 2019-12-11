bf354d7c77c889c7e2dc0be0fc29b6a54011ce9b
Jin Shin <jinshin1013@gmail.com>
2019-12-11 18:23:17 +1000

    Replacing lodash chain with flow (#5755)

--- scripts/test-js.js
+++ scripts/test-js.js
@@ -1,5 +1,11 @@
 const exec = require('shell-utils').exec;
-const { includes, chain, split, filter } = require('lodash');
+const includes = require('lodash/includes')
+const split = require('lodash/split')
+const filter = require('lodash/filter')
+
+const flow = require('lodash/fp/flow')
+const map = require('lodash/fp/map')
+const join = require('lodash/fp/join')
 
 const fix = includes(process.argv, '--fix') ? '--fix' : '';
 
@@ -14,7 +20,10 @@ const dirs = [
 run();
 
 function run() {
-  const paths = chain(dirs).map((d) => d === 'e2e' ? `${d}/**/*.[tj]s` : `${d}/**/*.[tj]sx?`).join(' ').value();
+  const paths = flow(
+    map((d) => d === 'e2e' ? `${d}/**/*.[tj]s` : `${d}/**/*.[tj]sx?`),
+    join(' ')
+  )(dirs)
   exec.execSync(`tslint ${paths} ${fix} --format verbose`);
   assertAllTsFilesInSrc();
   exec.execSync(`jest --coverage`);
