b01629c41da9197ee0737c937c02684c73dd9042
Yogev Ben David <yogev132@gmail.com>
2020-03-10 16:09:45 +0200

    Fix truncated bottomTab.text with semibold fontWeight (#6026)

--- lib/ios/TabBarItemAppearanceCreator.m
+++ lib/ios/TabBarItemAppearanceCreator.m
@@ -9,10 +9,12 @@
 }
 
 + (void)setTitleAttributes:(UITabBarItem *)tabItem titleAttributes:(NSDictionary *)titleAttributes {
+    [super setTitleAttributes:tabItem titleAttributes:titleAttributes];
     tabItem.standardAppearance.stackedLayoutAppearance.normal.titleTextAttributes = titleAttributes;
 }
 
 + (void)setSelectedTitleAttributes:(UITabBarItem *)tabItem selectedTitleAttributes:(NSDictionary *)selectedTitleAttributes {
+    [super setSelectedTitleAttributes:tabItem selectedTitleAttributes:selectedTitleAttributes];
     tabItem.standardAppearance.stackedLayoutAppearance.selected.titleTextAttributes = selectedTitleAttributes;
 }
 
